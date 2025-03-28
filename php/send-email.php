d3a82ba3c03be793fdd2e30970847c6806e68618
akinyemi samuel <akinyemisamuel170@gmail.com>
2025-03-28 18:56:06 +0100

    refactor email sending functionality to use PHPMailer for improved reliability and security

--- php/send-email.php
+++ php/send-email.php
@@ -1,48 +1,47 @@
 <?php
+require '../config/mail.php';
 
-// Replace this with your own email address
-$to = 'media@fashshotit.com';
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
-function url(){
-  return sprintf(
-    "%s://%s",
-    isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off' ? 'https' : 'http',
-    $_SERVER['SERVER_NAME']
-  );
-}
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
 
-if($_POST) {
+$name = $_POST["name"];
+$from = $_POST["email"];
+$message = $_POST["message"];
 
-   $name = trim(stripslashes($_POST['name']));
-   $email = trim(stripslashes($_POST['email']));
-   $subject = trim(stripslashes($_POST['subject']));
-   $contact_message = trim(stripslashes($_POST['message']));
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_OFF;
+    $mail->isSMTP();
+    $mail->Host = SMTP_HOST;
+    $mail->SMTPAuth = true;
+    $mail->Username = SMTP_USERNAME;
+    $mail->Password = SMTP_PASSWORD;
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS; //Enable implicit TLS encryption
+    $mail->Port = SMTP_PORT; //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
-   
-	if ($subject == '') { $subject = "Contact Form Submission"; }
+    //Recipients
+    $mail->AddReplyTo($from, $name);
+    $mail->setFrom(SMTP_USERNAME, $name);
+    $mail->addAddress('media@fashshotit.com', 'Fash Shot It Media'); //Add a recipient
 
-   // Set Message
-   $message .= "Email from: " . $name . "<br />";
-	 $message .= "Email address: " . $email . "<br />";
-   $message .= "Message: <br />";
-   $message .= nl2br($contact_message);
-   $message .= "<br /> ----- <br /> This email was sent from your site " . url() . " contact form. <br />";
+    //Content
+    $mail->isHTML(true); //Set email format to HTML
+    $mail->Subject = "New Booking from $name";
+    $mail->Body = "<h5>Sender Name: $name</h5>
+                    <p>Email: $from</p>
+                    <p>Message: $message</p>";
+    $mail->AltBody = "Sender Name: $name,\n email: $from, $message";
 
-   // Set From: header
-   $from =  $name . " <" . $email . ">";
-
-   // Email Headers
-	$headers = "From: " . $from . "\r\n";
-	$headers .= "Reply-To: ". $email . "\r\n";
- 	$headers .= "MIME-Version: 1.0\r\n";
-	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
-
-   ini_set("sendmail_from", $to); // for windows server
-   $mail = mail($to, $subject, $message, $headers);
-
-	if ($mail) { echo "OK"; }
-   else { echo "Something went wrong. Please try again."; }
-
+    $mail->send();
+    echo 'OK';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
 
 ?>
